acde624934efc66340d1411bea974ab43ba21d84
uhee <103394624+iamuheee@users.noreply.github.com>
2022-07-31 01:13:38 +0900

    Add : Company(회사소개, 이용약관 관련) VO 추가

---
+++ src/main/java/com/us/cs/homepage/model/vo/Company.java
@@ -0,0 +1,76 @@
+package com.us.cs.homepage.model.vo;
+
+import java.util.Date;
+
+public class Company {
+	private int comNo;
+	private int comCategory;
+	private int comContent;
+	private Date comUpdateDate;
+	
+	
+	public Company() {
+		super();
+	}
+
+	public Company(int comNo, int comCategory, int comContent, Date comUpdateDate) {
+		super();
+		this.comNo = comNo;
+		this.comCategory = comCategory;
+		this.comContent = comContent;
+		this.comUpdateDate = comUpdateDate;
+	}
+
+
+	public int getComNo() {
+		return comNo;
+	}
+
+
+	public void setComNo(int comNo) {
+		this.comNo = comNo;
+	}
+
+
+	public int getComCategory() {
+		return comCategory;
+	}
+
+
+	public void setComCategory(int comCategory) {
+		this.comCategory = comCategory;
+	}
+
+
+	public int getComContent() {
+		return comContent;
+	}
+
+
+	public void setComContent(int comContent) {
+		this.comContent = comContent;
+	}
+
+
+	public Date getComUpdateDate() {
+		return comUpdateDate;
+	}
+
+
+	public void setComUpdateDate(Date comUpdateDate) {
+		this.comUpdateDate = comUpdateDate;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Company [comNo=" + comNo + ", comCategory=" + comCategory + ", comContent=" + comContent
+				+ ", comUpdateDate=" + comUpdateDate + "]";
+	}	
+	
+	
+	
+	
+	
+
+}
