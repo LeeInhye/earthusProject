29bb7ea61180669a7c60399daad78313f57f40d8
ssuminee <rntnalseee@gmail.com>
2022-07-31 21:07:45 +0900

    update : Member 생성자 추가
    author : sumin

--- src/main/java/com/us/member/model/vo/Member.java
+++ src/main/java/com/us/member/model/vo/Member.java
@@ -42,8 +42,26 @@ public class Member {
 		this.userModifyDate = userModifyDate;
 		this.userStatus = userStatus;
 	}
+	
+	
 
 	
+
+	public Member(String userId, String userPwd, String userName, String email, String phone, String zonecode,
+			String address, String addrExtra, String addrDetail, String userBirth, String gender) {
+		super();
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.userName = userName;
+		this.email = email;
+		this.phone = phone;
+		this.zonecode = zonecode;
+		this.address = address;
+		this.addrExtra = addrExtra;
+		this.addrDetail = addrDetail;
+		this.userBirth = userBirth;
+		this.gender = gender;
+	}
 
 	public Member(String userId, String userPwd, String userName, String email, String phone,
 			String zonecode, String address, String addrExtra, String addrDetail, String userBirth, String gender,
