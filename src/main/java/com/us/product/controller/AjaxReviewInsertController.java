fba2a609d6784f851063cca8f6edcc7c23df72ee
uhee <103394624+iamuheee@users.noreply.github.com>
2022-08-02 14:54:03 +0900

    Fix : 리뷰 관련 수정 (미완성, 별거아님)

--- src/main/java/com/us/product/controller/AjaxReviewInsertController.java
+++ src/main/java/com/us/product/controller/AjaxReviewInsertController.java
@@ -1,11 +1,15 @@
 package com.us.product.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.us.product.model.service.ReviewService;
+import com.us.product.model.vo.Review;
 
 /**
  * Servlet implementation class AjaxReviewInsertController
@@ -26,7 +30,27 @@ public class AjaxReviewInsertController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 내일하자
+		
+		
+		// 리뷰 객체 채우기, 요청처리
+		Review r = new Review();
+		r.setUserNo( Integer.parseInt(request.getParameter("userNo")) );
+		r.setProCode( request.getParameter("proCode") );
+		r.setRevRate( Integer.parseInt(request.getParameter("revRate")) );
+		r.setRevContent( request.getParameter("revContent") );
+		r.setRevType("T");
+		
+		int result = new ReviewService().insertReview(r);
+		
+		// 리뷰 파일첨부했을 때에는...?
+		if( request.getParameter("photo") != null) {
+			
+		}
+		Attachment at = new Attachment();
+		
+		response.getWriter().print(result);
+		
+		
 	}
 
 	/**
--- src/main/java/com/us/product/model/service/ReviewService.java
+++ src/main/java/com/us/product/model/service/ReviewService.java
@@ -1,7 +1,20 @@
 package com.us.product.model.service;
 
+import java.sql.Connection;
+
 import static com.us.common.JDBCTemplate.*;
+import com.us.product.model.dao.ReviewDao;
+import com.us.product.model.vo.Review;
 
 public class ReviewService {
-
+	
+	// 첨부파일 없는 경우 insertReview 메소드
+	public int insertReview(Review r) {
+		Connection conn = getConnection();
+		int result = new ReviewDao().insertReview(conn, r);
+	}
+	
+	
+	
+	
 }
