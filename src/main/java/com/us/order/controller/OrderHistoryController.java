07c520aa4a2fc1022294892e7a471b112b9d2410
LeeInhye <103404692+LeeInhye@users.noreply.github.com>
2022-08-03 18:44:03 +0900

    Feat: 주문조회 페이징 처리 추가
    author: inhye

--- src/main/java/com/us/order/controller/OrderHistoryController.java
+++ src/main/java/com/us/order/controller/OrderHistoryController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.us.common.model.vo.PageInfo;
 import com.us.member.model.vo.Member;
 import com.us.order.model.service.OrderService;
 import com.us.order.model.vo.Order;
@@ -33,9 +34,37 @@ public class OrderHistoryController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
+		
+		// 페이징처리
+		int listCount;		
+		int currentPage;	
+		int pageLimit;		
+		int boardLimit;		
+		int maxPage;		
+		int startPage;		
+		int endPage;
+		
+		listCount = new OrderService().selectListCount();
+		
+		currentPage = Integer.parseInt(request.getParameter("opage"));
+		
+		pageLimit = 5;
+		boardLimit = 5;
+		maxPage = (int)Math.ceil( (double)listCount / boardLimit );
+		startPage = (currentPage-1) / pageLimit * pageLimit + 1;
+		endPage = startPage + pageLimit - 1;
+		
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
+		
+		PageInfo pi = new PageInfo(listCount, currentPage, pageLimit, boardLimit, maxPage, startPage, endPage);
+		
 		int userNo = ((Member)request.getSession().getAttribute("loginUser")).getUserNo();
 		
-		ArrayList<Order> list = new OrderService().selectOrderList(userNo);
+		ArrayList<Order> list = new OrderService().selectOrderList(pi, userNo);
+		
+		request.setAttribute("pi", pi);
 		request.getSession().setAttribute("list", list);
 		request.getRequestDispatcher("views/order/orderHistoryView.jsp").forward(request, response);
 		
